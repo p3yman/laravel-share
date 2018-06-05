20768564ae868ed53fa82deecd8b72ecd678ea92
Peyman Eskandari <peyman.eskandari@gmail.com>
2018-06-05 12:32:22 +0430

    Initial commit

---
+++ src/ShareServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace Peyman3d\Share;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShareServiceProvider extends ServiceProvider{
+	
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+	
+	}
+	
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+	
+	}
+
+}
