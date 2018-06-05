90fa808c86b77b7952b83ff6e0d08ddb4a06dd04
Peyman Eskandari <peyman.eskandari@gmail.com>
2018-06-05 13:11:52 +0430

    Share service and facade

---
+++ src/Share.php
@@ -0,0 +1,30 @@
+<?php
+namespace Peyman3d\Share;
+
+
+class Share {
+	
+	
+	/**
+	 * Share constructor.
+	 */
+	public function __construct() {
+	
+	}
+	
+	/**
+	 * Return $this for helper function
+	 *
+	 * @return $this
+	 */
+	public function this(){
+		return $this;
+	}
+	
+	/**
+	 * Test working
+	 */
+	public function test(){
+		dd('test');
+	}
+}
---
+++ src/ShareFacade.php
@@ -0,0 +1,18 @@
+<?php
+namespace Peyman3d\Share;
+
+use Illuminate\Support\Facades\Facade;
+
+class ShareFacade extends Facade {
+	
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+	{
+		return 'share';
+	}
+	
+}
